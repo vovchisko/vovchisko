f8022fd1f31a79b0064558141c443a4df2ca1f4e
Vladimir Ishenko <vovchisko@gmail.com>
2021-05-07 03:48:23 +0300

    font seems heavy :c

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import app           from './app.vue'
 import router        from './router'
 
-// import '/public/fonts/stylesheet.css'
+import '/public/fonts/stylesheet.css'
 import './root.scss'
 
 createApp(app).use(router).mount('#app')
