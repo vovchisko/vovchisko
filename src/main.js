1de42ab8a00dc2370dae15c1d29f898cbc7a6cf6
Vladimir Ishenko <vovchisko@gmail.com>
2021-05-07 03:55:50 +0300

    screw this font

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import app           from './app.vue'
 import router        from './router'
 
-import '/public/fonts/stylesheet.css'
 import './root.scss'
 
 createApp(app).use(router).mount('#app')
